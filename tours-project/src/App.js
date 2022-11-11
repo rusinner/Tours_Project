d12ed5ceb8a1f1e9aa2f461ab6c6db704f8b6877
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-11 21:13:15 +0200

    build each Tour

--- tours-project/src/App.js
+++ tours-project/src/App.js
@@ -1,7 +1,44 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
 import "./App.css";
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <div className="App">
+      <div className="tours">
+        <Tours tours={tours} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
